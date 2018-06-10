ecb870705df79e0ea2a2b1afbff93d35fc7470f3
dwj <dwi@163.com>
2018-06-10 22:06:55 +0800

    koa-lean

---
+++ controllers/hello.js
@@ -0,0 +1,11 @@
+/**
+ *  Created by daiwenjuan on 2018/6/10 21:37.
+ */
+var fn_hello = async (ctx, next) => {
+  var name = ctx.params.name
+  ctx.response.body = `<h1>Hello, ${name}!</h1>`
+}
+
+module.exports = {
+  'GET /hello/:name': fn_hello
+}
