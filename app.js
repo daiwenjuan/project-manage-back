4831fa29a0a423554662e36f0e42567f618c75fb
daiwenjuan <daiwenjuan668@163.com>
2018-06-12 16:28:08 +0800

    xlsx 文件上传

--- app.js
+++ app.js
@@ -1,25 +1,65 @@
 /**
  *  Created by daiwenjuan on 2018/6/10 16:28.
  */
-const Koa = require('koa')
+const koa = require('koa')
+const router = require('koa-router')()
+const app = new koa()
+const multer = require('koa-multer')
+const xlsx = require('xlsx')
+let formidable = require('formidable')
+let node_xlsx = require('node-xlsx')
+let path = require('path')
+let fs = require('fs')
 
-const bodyParser = require('koa-bodyparser')
-
-const controller = require('./controller')
-
-const app = new Koa()
-
-// log request URL:
-app.use(async (ctx, next) => {
-  console.log(`Process ${ctx.request.method} ${ctx.request.url}...`)
-  await next()
+router.get('/', async (ctx, next) => {
+  ctx.body = `<form action="http://127.0.0.1:3000/upload" method="post" enctype="multipart/form-data">  
+     <input type="file" name="file" value="选择文件"/>  
+     <input  type="submit"  value="上传"/>  
+</form>  `
 })
 
-// parse request body:
-app.use(bodyParser())
+let ExcelParse = function (newPath) {
+  let obj = node_xlsx.parse(newPath)
+  console.log(obj)
+}
 
-// add controllers:
-app.use(controller())
+router.post('/upload', async (ctx, next) => {
+  let form = new formidable.IncomingForm()
+  form.encoding = 'utf-8'
+  form.uploadDir = path.join(__dirname, '/public/uploads/')
+  form.keepExtensions = true//保留后缀
+  form.maxFieldsSize = 2 * 1024 * 1024
+  form.parse(ctx.req, function (err, fields, files) {
+    if (err) {
+      console.log('文件上传错误！')
+      return
+    }
+    let filename = files.file.name
 
-app.listen(3000)
-console.log('app started at port 3000...')
+    // 对文件名进行处理，以应对上传同名文件的情况
+    let nameArray = filename.split('.')
+    let type = nameArray[nameArray.length - 1]
+    let name = ''
+    for (let i = 0; i < nameArray.length - 1; i++) {
+      name = name + nameArray[i]
+    }
+
+    let date = new Date()
+    let time = '_' + date.getFullYear() + '_' + date.getMonth() + '_' + date.getDay() + '_' + date.getHours() + '_' +
+               date.getMinutes()
+
+    let avatarName = name + time + '.' + type
+
+    let newPath = form.uploadDir + avatarName
+    console.log(newPath)
+    fs.renameSync(files.file.path, newPath)  //重命名
+    console.log('重命名成功！')
+    ExcelParse(newPath)
+  })
+  ctx.body = '上传成功'
+})
+
+app.use(router.routes())
+app.listen(3000, () => {
+  console.log('starting at port 3000')
+})
