8a28a50ad585d8b015e1e6e101c8d21f5e26e3fb
daiwenjuan <daiwenjuan668@163.com>
2018-06-27 19:31:00 +0800

    删除没用代码

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const config = require('./webpack.config')
 const compiler = webpack(config)
 const devMiddleware = require('koa-webpack-dev-middleware')
 const hotMiddleware = require('koa-webpack-hot-middleware')
-const koaConvert = require('koa-convert')
 import clientRoute from './servers/middlewares/clientRoute'
 
 compiler.plugin('emit', (compilation, callback) => {
@@ -29,26 +28,12 @@ compiler.plugin('emit', (compilation, callback) => {
   })
   callback()
 })
-app.use(koaConvert(devMiddleware(compiler)))
-app.use(koaConvert(hotMiddleware(compiler)))
-
+app.use(devMiddleware(compiler))
+app.use(hotMiddleware(compiler))
 // 将/public文件夹设置为静态路径
 app.use(require('koa-static')(__dirname + '/public'))
 // 将ejs设置为我们的模板引擎
 app.use(views(path.resolve(__dirname, './views'), { map: { html: 'ejs' } }))
-// response
-// router.get('*', async (ctx) => {
-//   let str = renderToString(<Demo />)
-//   await ctx.render('index', {
-//     root: str
-//   })
-// })
-// app.use(async ctx => {
-//   let str = renderToString(<Demo />)
-//   await ctx.render('index', {
-//     root: str
-//   })
-// })
 app.use(clientRoute)
 app.use(router.routes())
 app.use(router.allowedMethods())
