4d533db86688322c3e58d7a5186c7a58fdaa707b
daiwenjuan <daiwenjuan668@163.com>
2018-07-02 16:57:48 +0800

    调试

--- servers/server.js
+++ servers/server.js
@@ -17,28 +17,28 @@ const hotMiddleware = require('koa-webpack-hot-middleware')
 import clientRoute from './middlewares/clientRoute'
 import logger from 'koa-logger'
 
-// compiler.plugin('emit', (compilation, callback) => {
-//   const assets = compilation.assets
-//   let file, data
-//   Object.keys(assets).forEach(key => {
-//     if (key.match(/\.html$/)) {
-//       file = path.resolve(__dirname, key)
-//       data = assets[key].source()
-//       fs.writeFileSync(file, data)
-//     }
-//   })
-//   callback()
-// })
+compiler.plugin('emit', (compilation, callback) => {
+  const assets = compilation.assets
+  let file, data
+  Object.keys(assets).forEach(key => {
+    if (key.match(/\.html$/)) {
+      file = path.resolve(__dirname, key)
+      data = assets[key].source()
+      fs.writeFileSync(file, data)
+    }
+  })
+  callback()
+})
 app.use(logger())
-// app.use(devMiddleware(compiler, {
-//   noInfo: false,
-//   publicPath: config.output.publicPath
-// }))
-// app.use(hotMiddleware(compiler))
+app.use(devMiddleware(compiler, {
+  noInfo: false,
+  publicPath: config.output.publicPath
+}))
+app.use(hotMiddleware(compiler))
 // 将/public文件夹设置为静态路径
-app.use(require('koa-static')(path.resolve(__dirname + '../dist/client')))
+// app.use(require('koa-static')(path.resolve(__dirname + '../dist/client')))
 // 将ejs设置为我们的模板引擎
-app.use(views(path.resolve(__dirname, '../views/pro'), { map: { html: 'ejs' } }))
+app.use(views(path.resolve(__dirname, '../views/dev'), { map: { html: 'ejs' } }))
 app.use(clientRoute)
 app.use(router.routes())
 app.use(router.allowedMethods())
